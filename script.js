e8c4af82a16c1109fab09a4ca55f4bc535112b50
Aeson12 <aesonindustries@gmail.com>
2025-01-02 16:12:57 +0000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./sw.js")
-    .then((registration) => console.log("SW registered:", registration))
-    .catch((error) => console.error("SW registration failed:", error));
-}
