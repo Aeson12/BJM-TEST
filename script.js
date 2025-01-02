6bcd9f655315ded6e1a05db76fab781c742d1eb9
Aeson12 <aesonindustries@gmail.com>
2025-01-02 15:26:46 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-T
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("./sw.js")
+    .then((registration) => console.log("SW registered:", registration))
+    .catch((error) => console.error("SW registration failed:", error));
+}
