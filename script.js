a78f2145aa428131fd471e39312084130e7b6af3
Aeson12 <aesonindustries@gmail.com>
2025-01-02 23:29:02 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ if ("serviceWorker" in navigator) {
     .then((registration) => console.log("SW registered:", registration))
     .catch((error) => console.error("SW registration failed:", error));
 }
+
+const notificationsToggle = document.getElementById("notifications-toggle");
+notificationsToggle.addEventListener("change", async (event) => {
+  if (event.target.checked) {
+    if (!("Notification" in window)) {
+      alert("This browser does not support notifications.");
+      return;
+    }
+
+    const permission = await Notification.requestPermission();
+    if (permission === "granted") {
+      new Notification("Notifications enabled! 🎉");
+    } else {
+      alert("Notifications denied.");
+      event.target.checked = false;
+    }
+  }
+});
